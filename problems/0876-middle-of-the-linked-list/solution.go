95363eee64ffe67e3b9e149cd66fbac2978b390f
zulerne <165476940+zulerne@users.noreply.github.com>
2026-04-22 21:00:37 +0700

    feat: add 876

---
+++ problems/0876-middle-of-the-linked-list/solution.go
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/middle-of-the-linked-list/description/
+package middleofthelinkedlist
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func middleNode(head *ListNode) *ListNode {
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+	return slow
+}
