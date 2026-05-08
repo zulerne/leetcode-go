869eb999132d1092648d1444c35f7d9a9b20643c
zulerne <165476940+zulerne@users.noreply.github.com>
2026-05-08 15:40:23 +0700

    feat: add 253

---
+++ problems/0253-meeting-rooms-ii/solution.go
@@ -0,0 +1,54 @@
+// https://leetcode.com/problems/meeting-rooms-ii/description/
+package meetingroomsii
+
+import (
+	"container/heap"
+	"sort"
+)
+
+type Interval struct {
+	Start, End int
+}
+
+type MinHeap []*Interval
+
+func (h MinHeap) Len() int {
+	return len(h)
+}
+
+func (h MinHeap) Less(i, j int) bool {
+	return h[i].End < h[j].End
+}
+
+func (h MinHeap) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+}
+
+func (h *MinHeap) Push(x any) {
+	*h = append(*h, x.(*Interval))
+}
+
+func (h *MinHeap) Pop() any {
+	old := *h
+	x := old[len(old)-1]
+	*h = old[:len(old)-1]
+	return x
+}
+
+// TODO: Repeat
+func MinMeetingRooms(intervals []*Interval) int {
+	sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i].Start < intervals[j].Start
+	})
+	h := MinHeap{}
+	heap.Init(&h)
+
+	for _, iv := range intervals {
+		if h.Len() > 0 && h[0].End <= iv.Start {
+			heap.Pop(&h)
+		}
+		heap.Push(&h, iv)
+	}
+
+	return h.Len()
+}
