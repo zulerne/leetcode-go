49bc2a356de573f1061933f88258c9b8203ddc0c
zulerne <165476940+zulerne@users.noreply.github.com>
2026-05-11 19:25:22 +0700

    feat: update 200

--- problems/0200-number-of-islands/solution.go
+++ problems/0200-number-of-islands/solution.go
@@ -2,8 +2,7 @@
 package numberofislands
 
 func numIslands(grid [][]byte) int {
-	var res int
-	n, m := len(grid), len(grid[0])
+	var islands int
 	dirs := [][2]int{{0, 1}, {0, -1}, {1, 0}, {-1, 0}}
 
 	for i := range grid {
@@ -11,18 +10,18 @@ func numIslands(grid [][]byte) int {
 			if grid[i][j] == '0' {
 				continue
 			}
-			grid[i][j] = '0'
-			res++
+
+			islands++
 
 			queue := [][2]int{{i, j}}
 			for len(queue) > 0 {
-				first := queue[0]
+				ii, jj := queue[0][0], queue[0][1]
 				queue = queue[1:]
-				ii, jj := first[0], first[1]
+				grid[ii][jj] = '0'
 
 				for _, d := range dirs {
 					ni, nj := ii+d[0], jj+d[1]
-					if ni >= 0 && ni < n && nj >= 0 && nj < m && grid[ni][nj] == '1' {
+					if ni >= 0 && ni < len(grid) && nj >= 0 && nj < len(grid[ni]) && grid[ni][nj] == '1' {
 						grid[ni][nj] = '0'
 						queue = append(queue, [2]int{ni, nj})
 					}
@@ -31,5 +30,5 @@ func numIslands(grid [][]byte) int {
 		}
 	}
 
-	return res
+	return islands
 }
