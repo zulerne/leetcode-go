73fedadfd253ebf6f21c0855213dad85390d1336
zulerne <165476940+zulerne@users.noreply.github.com>
2026-05-29 13:54:36 +0700

    feat: add 2115

---
+++ problems/2115-find-all-possible-recipes-from-given-supplies/solution.go
@@ -0,0 +1,33 @@
+// https://leetcode.com/problems/find-all-possible-recipes-from-given-supplies/description/
+package findallpossiblerecipesfromgivensupplies
+
+func findAllRecipes(recipes []string, ingredients [][]string, supplies []string) []string {
+	res := make([]string, 0)
+	inDegree := make(map[string]int, len(recipes))
+	g := make(map[string][]string, len(supplies)+len(recipes))
+
+	for i, r := range recipes {
+		for _, ing := range ingredients[i] {
+			g[ing] = append(g[ing], r)
+			inDegree[r]++
+		}
+	}
+
+	queue := make([]string, len(supplies))
+	copy(queue, supplies)
+	for len(queue) > 0 {
+		sup := queue[0]
+		queue = queue[1:]
+
+		for _, v := range g[sup] {
+			inDegree[v]--
+
+			if inDegree[v] == 0 {
+				queue = append(queue, v)
+				res = append(res, v)
+			}
+		}
+	}
+
+	return res
+}
