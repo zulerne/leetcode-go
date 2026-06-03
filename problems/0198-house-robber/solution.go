2ba07187d403d1c981956c12c534f27119be6b27
zulerne <165476940+zulerne@users.noreply.github.com>
2026-06-03 16:54:19 +0700

    feat: update 198

--- problems/0198-house-robber/solution.go
+++ problems/0198-house-robber/solution.go
@@ -2,13 +2,23 @@
 package houserobber
 
 func rob(nums []int) int {
-	n := len(nums)
-	dp := make([]int, n+1)
-	dp[1] = nums[0]
-
-	for i := 2; i < len(dp); i++ {
-		dp[i] = max(nums[i-1]+dp[i-2], dp[i-1])
+	a, b := 0, 0
+	for _, n := range nums {
+		tmp := max(a+n, b)
+		a = b
+		b = tmp
 	}
-
-	return dp[n]
+	return b
 }
+
+// func rob(nums []int) int {
+// 	n := len(nums)
+// 	dp := make([]int, n+1)
+// 	dp[1] = nums[0]
+
+// 	for i := 2; i < len(dp); i++ {
+// 		dp[i] = max(nums[i-1]+dp[i-2], dp[i-1])
+// 	}
+
+// 	return dp[n]
+// }
