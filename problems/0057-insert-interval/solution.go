77263119b7cf9f0063433a27a3f39fa043411ec1
zulerne <165476940+zulerne@users.noreply.github.com>
2026-06-24 11:59:52 +0700

    feat: update 57

--- problems/0057-insert-interval/solution.go
+++ problems/0057-insert-interval/solution.go
@@ -2,21 +2,24 @@
 package insertinterval
 
 func insert(intervals [][]int, newInterval []int) [][]int {
-	result := make([][]int, 0)
+	n := len(intervals)
 	i := 0
 
-	for ; i < len(intervals) && intervals[i][1] < newInterval[0]; i++ {
-		result = append(result, intervals[i])
+	res := make([][]int, 0)
+
+	for ; i < n && intervals[i][1] < newInterval[0]; i++ {
+		res = append(res, intervals[i])
 	}
-	for ; i < len(intervals) && intervals[i][0] <= newInterval[1]; i++ {
+
+	for ; i < n && intervals[i][0] <= newInterval[1]; i++ {
 		newInterval[0] = min(newInterval[0], intervals[i][0])
 		newInterval[1] = max(newInterval[1], intervals[i][1])
 	}
-	result = append(result, newInterval)
+	res = append(res, newInterval)
 
-	for ; i < len(intervals); i++ {
-		result = append(result, intervals[i])
+	for ; i < n; i++ {
+		res = append(res, intervals[i])
 	}
 
-	return result
+	return res
 }
