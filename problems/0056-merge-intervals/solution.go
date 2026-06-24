9d62eb758d1f8f4d8cd58cfca7e39747ed45a665
zulerne <165476940+zulerne@users.noreply.github.com>
2026-06-24 11:12:56 +0700

    feat: update 56

--- problems/0056-merge-intervals/solution.go
+++ problems/0056-merge-intervals/solution.go
@@ -1,42 +1,27 @@
 // https://leetcode.com/problems/merge-intervals/description/
 package mergeintervals
 
-import "sort"
-
-func mergeAlternative(intervals [][]int) [][]int {
-	sort.Slice(intervals, func(i, j int) bool {
-		return intervals[i][0] < intervals[j][0]
-	})
-	result := make([][]int, 0)
-
-	for i := 1; i <= len(intervals); i++ {
-		start := intervals[i-1][0]
-		end := intervals[i-1][1]
-		for i < len(intervals) && intervals[i][0] <= end {
-			end = max(end, intervals[i][1])
-			i++
-		}
-		result = append(result, []int{start, end})
-	}
-
-	return result
-}
+import (
+	"slices"
+)
 
 func merge(intervals [][]int) [][]int {
-	sort.Slice(intervals, func(i, j int) bool {
-		return intervals[i][0] < intervals[j][0]
+	n := len(intervals)
+	res := make([][]int, 0, n)
+
+	slices.SortFunc(intervals, func(a, b []int) int {
+		return a[0] - b[0]
 	})
 
-	result := [][]int{intervals[0]}
+	for i := 0; i < n; i++ {
+		a, b := intervals[i][0], intervals[i][1]
 
-	for i := 1; i < len(intervals); i++ {
-		last := result[len(result)-1]
-		if intervals[i][0] <= last[1] {
-			result[len(result)-1][1] = max(result[len(result)-1][1], intervals[i][1])
-		} else {
-			result = append(result, intervals[i])
+		for i+1 < n && b >= intervals[i+1][0] {
+			b = max(b, intervals[i+1][1])
+			i++
 		}
+		res = append(res, []int{a, b})
 	}
 
-	return result
+	return res
 }
