7e26cabf7a45f4c512641a4eb0d5237787388018
zulerne <165476940+zulerne@users.noreply.github.com>
2026-06-26 13:42:11 +0700

    feat: update 141

--- problems/0141-linked-list-cycle/solution.go
+++ problems/0141-linked-list-cycle/solution.go
@@ -9,8 +9,7 @@ type ListNode struct {
 func hasCycle(head *ListNode) bool {
 	fast, slow := head, head
 	for fast != nil && fast.Next != nil {
-		fast = fast.Next.Next
-		slow = slow.Next
+		fast, slow = fast.Next.Next, slow.Next
 		if fast == slow {
 			return true
 		}
