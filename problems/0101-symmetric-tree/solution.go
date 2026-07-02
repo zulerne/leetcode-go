f1829a0099b0564fdfb2bc33bc9409040b2ba9f7
zulerne <165476940+zulerne@users.noreply.github.com>
2026-07-02 14:31:32 +0700

    feat: update 101

--- problems/0101-symmetric-tree/solution.go
+++ problems/0101-symmetric-tree/solution.go
@@ -7,38 +7,19 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
-func isSymmetricIterative(root *TreeNode) bool {
-	stack := []*TreeNode{root.Left, root.Right}
-
-	for len(stack) >= 2 {
-		r := stack[len(stack)-1]
-		stack = stack[:len(stack)-1]
-
-		l := stack[len(stack)-1]
-		stack = stack[:len(stack)-1]
-
-		if r == l && r == nil {
-			continue
-		}
-		if r == nil || l == nil || r.Val != l.Val {
-			return false
-		}
-		stack = append(stack, r.Left, l.Right, r.Right, l.Left)
-	}
-
-	return len(stack) == 0
-}
-
 func isSymmetric(root *TreeNode) bool {
 	var check func(l, r *TreeNode) bool
 	check = func(l, r *TreeNode) bool {
-		if r == l && r == nil {
+		if l == nil && r == nil {
 			return true
 		}
-		if r == nil || l == nil || r.Val != l.Val {
+
+		if l == nil || r == nil || l.Val != r.Val {
 			return false
 		}
-		return check(r.Right, l.Left) && check(r.Left, l.Right)
+
+		return check(l.Left, r.Right) && check(l.Right, r.Left)
 	}
+
 	return check(root.Left, root.Right)
 }
