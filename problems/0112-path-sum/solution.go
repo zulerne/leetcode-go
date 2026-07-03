4a9c98c2aa09d3e3fab6d1dfbac2a602c4a18c3d
zulerne <165476940+zulerne@users.noreply.github.com>
2026-07-03 15:49:11 +0700

    feat: update 112

--- problems/0112-path-sum/solution.go
+++ problems/0112-path-sum/solution.go
@@ -11,36 +11,10 @@ func hasPathSum(root *TreeNode, targetSum int) bool {
 	if root == nil {
 		return false
 	}
-	targetDiff := targetSum - root.Val
-	if targetDiff == 0 && root.Left == nil && root.Right == nil {
-		return true
+
+	if root.Right == nil && root.Left == nil {
+		return targetSum == root.Val
 	}
-	return hasPathSum(root.Left, targetDiff) || hasPathSum(root.Right, targetDiff)
-}
 
-type stackElem struct {
-	node    *TreeNode
-	current int
-}
-
-func hasPathSumIterative(root *TreeNode, targetSum int) bool {
-	stack := []stackElem{{node: root, current: 0}}
-	for len(stack) > 0 {
-		el := stack[len(stack)-1]
-		node, current := el.node, el.current
-		stack = stack[:len(stack)-1]
-
-		if node == nil {
-			continue
-		}
-
-		current += node.Val
-
-		if node.Left == nil && node.Right == nil && current == targetSum {
-			return true
-		}
-		stack = append(stack, stackElem{node: node.Left, current: current})
-		stack = append(stack, stackElem{node: node.Right, current: current})
-	}
-	return false
+	return hasPathSum(root.Left, targetSum-root.Val) || hasPathSum(root.Right, targetSum-root.Val)
 }
