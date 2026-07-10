bdaa908688d1139f5a286ee1bb229817e0aaaecd
zulerne <165476940+zulerne@users.noreply.github.com>
2026-07-10 16:29:31 +0700

    feat: update 77

--- problems/0077-combinations/solution.go
+++ problems/0077-combinations/solution.go
@@ -2,25 +2,29 @@
 package combinations
 
 func combine(n int, k int) [][]int {
-	var result [][]int
-	var backtrack func(acc []int, i int)
+	res := make([][]int, 0)
+	acc := make([]int, k)
 
-	backtrack = func(acc []int, cur int) {
-		if len(acc) == k {
-			tmp := make([]int, len(acc))
-			copy(tmp, acc)
-			result = append(result, tmp)
+	var backtrack func(start, pos int)
+	backtrack = func(start, pos int) {
+		if pos == k {
+			r := make([]int, k)
+			copy(r, acc)
+			res = append(res, r)
 			return
 		}
 
-		for i := cur; i < n+1; i++ {
-			acc = append(acc, i)
-			backtrack(acc, i+1)
-			acc = acc[:len(acc)-1]
+		need := k - pos
+		for cur := start + 1; cur <= n-need+1; cur++ {
+			if pos+1 == k+1 {
+				break
+			}
+			acc[pos] = cur
+			backtrack(cur, pos+1)
 		}
 	}
 
-	backtrack([]int{}, 1)
+	backtrack(0, 0)
 
-	return result
+	return res
 }
