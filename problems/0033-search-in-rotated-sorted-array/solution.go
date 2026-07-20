ea3782a8f3383e9627146f3a1a5b18ffe8b87708
zulerne <165476940+zulerne@users.noreply.github.com>
2026-07-20 21:22:13 +0700

    feat: add 33

---
+++ problems/0033-search-in-rotated-sorted-array/solution.go
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/search-in-rotated-sorted-array/description/
+package searchinrotatedsortedarray
+
+func search(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+
+	for left <= right {
+		mid := left + (right-left)/2
+
+		if nums[mid] == target {
+			return mid
+		}
+
+		if nums[mid] >= nums[left] {
+			if target >= nums[left] && target < nums[mid] {
+				right = mid - 1
+			} else {
+				left = mid + 1
+			}
+		} else {
+			if target > nums[mid] && target <= nums[right] {
+				left = mid + 1
+			} else {
+				right = mid - 1
+			}
+		}
+	}
+
+	return -1
+}
