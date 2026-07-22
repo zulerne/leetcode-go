6f1670733e87db5781d2bb0d0076564a61fb9309
zulerne <165476940+zulerne@users.noreply.github.com>
2026-07-22 10:52:35 +0700

    feat: update 215

--- problems/0215-kth-largest-element-in-an-array/solution.go
+++ problems/0215-kth-largest-element-in-an-array/solution.go
@@ -1,84 +1,50 @@
 // https://leetcode.com/problems/kth-largest-element-in-an-array/description/
 package kthlargestelementinanarray
 
-import "container/heap"
-
-// INFO: MinHeap
+import (
+	"container/heap"
+)
 
 type MinHeap []int
 
-func (h MinHeap) Len() int {
-	return len(h)
+// Len implements [heap.Interface].
+func (m *MinHeap) Len() int {
+	return len(*m)
 }
 
-func (h MinHeap) Less(i, j int) bool {
-	return h[i] < h[j]
+// Less implements [heap.Interface].
+func (m *MinHeap) Less(i int, j int) bool {
+	return (*m)[i] < (*m)[j]
 }
 
-func (h MinHeap) Swap(i, j int) {
-	h[i], h[j] = h[j], h[i]
+// Pop implements [heap.Interface].
+func (m *MinHeap) Pop() any {
+	old := *m
+	last := old[len(old)-1]
+	*m = old[:len(old)-1]
+	return last
 }
 
-func (h *MinHeap) Push(x any) {
-	*h = append(*h, x.(int))
+// Push implements [heap.Interface].
+func (m *MinHeap) Push(x any) {
+	old := *m
+	*m = append(old, x.(int))
 }
 
-func (h *MinHeap) Pop() any {
-	old := *h
-	x := old[len(old)-1]
-	*h = old[:len(old)-1]
-	return x
+// Swap implements [heap.Interface].
+func (m *MinHeap) Swap(i int, j int) {
+	(*m)[i], (*m)[j] = (*m)[j], (*m)[i]
 }
 
 func findKthLargest(nums []int, k int) int {
 	h := MinHeap(nums[:k])
 	heap.Init(&h)
-
 	for _, num := range nums[k:] {
 		if num > h[0] {
 			heap.Pop(&h)
 			heap.Push(&h, num)
 		}
-
 	}
 
-	return h[0]
-}
-
-// INFO: MaxHeap
-
-type MaxHeap []int
-
-func (h MaxHeap) Len() int {
-	return len(h)
-}
-
-func (h MaxHeap) Less(i, j int) bool {
-	return h[i] > h[j]
-}
-
-func (h MaxHeap) Swap(i, j int) {
-	h[i], h[j] = h[j], h[i]
-}
-
-func (h *MaxHeap) Push(x any) {
-	*h = append(*h, x.(int))
-}
-
-func (h *MaxHeap) Pop() any {
-	old := *h
-	x := old[len(old)-1]
-	*h = old[:len(old)-1]
-	return x
-}
-
-func findKthLargestMaxHeap(nums []int, k int) int {
-	h := make(MaxHeap, len(nums))
-	copy(h, nums)
-	heap.Init(&h)
-
-	for range k - 1 {
-		heap.Pop(&h)
-	}
 	return h[0]
 }
